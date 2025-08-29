60cfc8d9f2a939adb7c97f9a7fc8b10805f0a775
meysam-safarzadeh <m.s.97.eng@gmail.com>
2025-08-29 23:50:25 +0200

    add: implement start action for TrainingJobViewSet to initiate training jobs

--- backend/registry/views.py
+++ backend/registry/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Dataset, DetectionResult, TrainingJob, Metric, Artifact
 from .serializers import (
     DatasetSerializer, DetectionResultSerializer, TrainingJobSerializer,
     MetricSerializer, ArtifactSerializer
 )
+from services.training_services import start_training_background
+
 
 class DatasetViewSet(viewsets.ModelViewSet):
     queryset = Dataset.objects.all()
@@ -16,6 +20,17 @@ class DetectionResultViewSet(viewsets.ModelViewSet):
 class TrainingJobViewSet(viewsets.ModelViewSet):
     queryset = TrainingJob.objects.all()
     serializer_class = TrainingJobSerializer
+
+    @action(detail=True, methods=["post"], url_path="start")
+    def start(self, request, pk=None):
+        # Optional: prevent duplicate starts
+        job = self.get_object()
+        if job.status in [TrainingJob.Status.RUNNING]:
+            return Response({"detail": "Job already running."}, status=status.HTTP_409_CONFLICT)
+
+        start_training_background(job.id)
+        return Response({"message": "Training started", "job_id": job.id}, status=status.HTTP_202_ACCEPTED)
+
 
 class MetricViewSet(viewsets.ModelViewSet):
     queryset = Metric.objects.all()
